88068a1dfea4da166aa78e718d09fbcb5648493b
ayillikci <alperenyillikci@gmail.com>
2024-09-24 14:29:53 +0200

    Init

--- products.py
+++ products.py
@@ -2,3 +2,8 @@ class Product:
     def __init__(self, name:str, price: float, quantity: int):
         if not name:
             raise ValueError("Product name cannot be empty.")
+        if price < 0 :
+            raise ValueError("Product can price can not be below 0")
+        if quantity < 0 :
+            raise ValueError("Product quantity cannot be negative.")
+
